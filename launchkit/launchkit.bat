e20e9ee4c4ef045d1d52a8ec2a1ef4fe991bf757
Daniel Mills <danielmillst@gmail.com>
2021-04-13 23:54:41 -0400

    No LFS

--- launchkit/launchkit.bat
+++ launchkit/launchkit.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar LaunchKit-1.8.jar !local_fs=true !download_threads=8 !forge_version=14.23.5.2847 !auto_start=true !pack=https://raw.githubusercontent.com/cyberpwnn/Aura/main/launchkit/pack.json
+java -jar LaunchKit-1.8.jar !download_threads=8 !forge_version=14.23.5.2847 !auto_start=true !pack=https://raw.githubusercontent.com/cyberpwnn/Aura/main/launchkit/pack.json
