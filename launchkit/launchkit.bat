a3c454da50024b50a4acb83615fd42c214528c47
Daniel Mills <danielmillst@gmail.com>
2021-04-13 23:39:42 -0400

    v

--- launchkit/launchkit.bat
+++ launchkit/launchkit.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar LaunchKit-1.8.jar !local_fs=true !download_threads=8 !pack=file://pack.json !forge_version=14.23.5.2854 !url_forge_universal=https://files.minecraftforge.net/maven/net/minecraftforge/forge/{game-version}-{forge-version}/forge-{game-version}-{forge-version}-installer.jar
+java -jar LaunchKit-1.8.jar !local_fs=true !download_threads=8 !pack=file://pack.json !forge_version=14.23.5.2847 !auto_start=true
